cacedbf0483c5cdb4b64161ec1303e55ca9ac93e
Vlad Dikoi <vdikoi@yandex.ru>
2020-03-22 16:42:40 +0300

    swiper-3

--- src/about.js
+++ src/about.js
@@ -2,4 +2,12 @@ import "./themes/about/about.css";
 
 import Swiper from 'swiper';
 import "../node_modules/swiper/css/swiper.min.css";
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
 import "../node_modules/swiper/js/swiper.min.js";
