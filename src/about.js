31b8b5cfc14eb24f71966ccd71ba852ea2964e8b
Vlad Dikoi <vdikoi@yandex.ru>
2020-03-22 16:39:11 +0300

    swiper-2

--- src/about.js
+++ src/about.js
@@ -1,1 +1,5 @@
 import "./themes/about/about.css";
+
+import Swiper from 'swiper';
+import "../node_modules/swiper/css/swiper.min.css";
+import "../node_modules/swiper/js/swiper.min.js";
--- webpack.config.js
+++ webpack.config.js
@@ -66,6 +66,6 @@ module.exports = {
                     preset: ['default'],
             },
             canPrint: true
-    })
+    }),
     ]
 };
