4efe403a1e44b6f35e1a1cbb0dbb454b7da80a5b
Vlad Dikoi <vdikoi@yandex.ru>
2020-04-02 17:31:10 +0300

    first js commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./themes/main/style.css";
+
+
+/* Валидация формы */
+const form = document.querySelector('.search__form');
+const submit = document.querySelector('.search__button');
+
+submit.addEventListener('click', sendForm);
+
+function validate(element) {
+    if(!element.checkValidity()) {
+        document.querySelector('.search__error').style.display = 'block';
+    }
+}
+
+function sendForm (event) {
+    event.preventDefault();
+    validate(form.elements.theme);
+}
+/* --------------- */
